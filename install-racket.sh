e68f332201ead542c900017a8e749cd2bc64ce8a
Greg Hendershott <greghendershott@gmail.com>
2013-07-02 14:35:42 -0400

    Install Racket using curl instead of apt-get

---
+++ install-racket.sh
@@ -0,0 +1,21 @@
+RACKET_VERSION=5.3.5
+RACKET_URL="http://download.racket-lang.org/installers/$VERSION/racket/racket-$VERSION-bin-x86_64-linux-f14.sh"
+RACKET_INSTALLER="racket-$VERSION.sh"
+
+echo "Downloading $RACKET_URL to $RACKET_INSTALLER..."
+curl -o $RACKET_INSTALLER $RACKET_URL
+
+if [[ ! -f $RACKET_INSTALLER ]]; then
+	echo "Error downloading Racket $VERSION from $RACKET_URL"
+	exit 1
+fi
+
+echo "Running Racket installer"
+chmod u+rx "$RACKET_INSTALLER"
+"$RACKET_INSTALLER" <<EOF
+no
+4
+
+EOF
+
+exit 0
