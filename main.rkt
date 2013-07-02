7ed7e0cd2b1fbf60e546b8141bb77e6dbf42fc7f
Greg Hendershott <greghendershott@gmail.com>
2013-07-02 14:16:01 -0400

    Add minus1

--- main.rkt
+++ main.rkt
@@ -3,6 +3,10 @@
 (define (plus1 x)
   (add1 x))
 
+(define (minus1 x)
+  (sub1 x))
+
 (module+ test
   (require rackunit)
-  (check-equal? (plus1 1) 2))
+  (check-equal? (plus1 1) 2)
+  (check-equal? (minus1 1) 0))
