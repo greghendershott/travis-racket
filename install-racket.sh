536f1ef1e1d93fc6659b6322da4c0a664f6eca2b
Greg Hendershott <greghendershott@gmail.com>
2013-07-02 18:51:03 -0400

    Move sudo from yml to sh. Tweak a message.

--- install-racket.sh
+++ install-racket.sh
@@ -4,12 +4,12 @@ VERSION=5.3.5
 URL="http://download.racket-lang.org/installers/$VERSION/racket/racket-$VERSION-bin-x86_64-linux-ubuntu-precise.sh"
 INSTALL="./racket-$VERSION.sh"
 
-echo "Downloading $URL to $INSTALL..."
+echo "Downloading $URL to $INSTALL:"
 curl -o $INSTALL $URL
 
-echo "Running Racket installer..."
+echo "Running $INSTALL to install Racket:"
 chmod u+rx "$INSTALL"
-"$INSTALL" <<EOF
+sudo "$INSTALL" <<EOF
 no
 1
 
