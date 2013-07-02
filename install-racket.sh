b92b7a47829feb5da65fc42f1b1c84c9c9bda6aa
Greg Hendershott <greghendershott@gmail.com>
2013-07-02 14:55:34 -0400

    With more dot slash

--- install-racket.sh
+++ install-racket.sh
@@ -1,6 +1,6 @@
 VERSION=5.3.5
 URL="http://download.racket-lang.org/installers/$VERSION/racket/racket-$VERSION-bin-x86_64-linux-f14.sh"
-INSTALL="racket-$VERSION.sh"
+INSTALL="./racket-$VERSION.sh"
 
 echo "Downloading $URL to $INSTALL..."
 curl -o $INSTALL $URL
