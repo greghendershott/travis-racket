0cba8d92b52f3e6d5b04e60d09de7438d83afc78
Greg Hendershott <greghendershott@gmail.com>
2013-10-20 05:39:07 -0400

    Use new abstract URL for HEAD nightly installers.
    re issue #3

--- install-racket.sh
+++ install-racket.sh
@@ -1,12 +1,7 @@
 set -e
 
 if [ "$RACKET_VERSION" = "HEAD" ]; then
-    # Unfortunately there is no abstract URL for "HEAD" -- the nighly
-    # builds have a version number like 5.90.0.9 embedded in the
-    # URL. So this will need to be updated manually whenever the
-    # version is bumped.
-    HEAD_VERSION="5.90.0.9"
-    URL="http://www.cs.utah.edu/plt/snapshots/current/installers/racket-${HEAD_VERSION}-x86_64-linux-precise.sh"
+    URL="http://www.cs.utah.edu/plt/snapshots/current/installers/racket-current-x86_64-linux-precise.sh"
 else
     URL="http://download.racket-lang.org/installers/${RACKET_VERSION}/racket/racket-${RACKET_VERSION}-bin-x86_64-linux-debian-squeeze.sh"
 fi
