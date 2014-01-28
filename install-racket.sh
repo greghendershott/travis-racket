e943dff124bc118c64f476e8dab29c50609aac7d
Greg Hendershott <greghendershott@gmail.com>
2014-01-28 15:07:46 -0500

    Special case 5.92

--- install-racket.sh
+++ install-racket.sh
@@ -2,6 +2,8 @@ set -e
 
 if [ "$RACKET_VERSION" = "HEAD" ]; then
     URL="http://www.cs.utah.edu/plt/snapshots/current/installers/racket-current-x86_64-linux-precise.sh"
+elif [ "$RACKET_VERSION" = "5.92" ]; then
+    URL="http://download.racket-lang.org/installers/5.92/racket-5.92-x86_64-linux-ubuntu-quantal.sh"
 else
     URL="http://download.racket-lang.org/installers/${RACKET_VERSION}/racket/racket-${RACKET_VERSION}-bin-x86_64-linux-debian-squeeze.sh"
 fi
