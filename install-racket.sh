e1d8cbf061a43b9ccde857f4932bab10adef75b1
Greg Hendershott <greghendershott@gmail.com>
2014-03-13 16:29:43 -0400

    Use -L with curl for download of Racket installer.
    To follow redirects, if any.

--- install-racket.sh
+++ install-racket.sh
@@ -16,7 +16,7 @@ fi
 INSTALL="./racket-${RACKET_VERSION}.sh"
 
 echo "Downloading $URL to $INSTALL:"
-curl -o $INSTALL $URL
+curl -L -o $INSTALL $URL
 
 echo "Running $INSTALL to install Racket:"
 chmod u+rx "$INSTALL"
