831a87d30ac85482274c28e0c0d6010beaca4ad0
Alexis King <lexi.lambda@gmail.com>
2016-05-03 00:29:10 -0700

    Update for 6.5 installer URLs
    fixes #14

--- install-racket.sh
+++ install-racket.sh
@@ -11,8 +11,10 @@ elif [[ "$RACKET_VERSION" = "RELEASE" ]]; then
     URL="http://pre-release.racket-lang.org/installers/racket-current-x86_64-linux-ubuntu-precise.sh"
 elif [[ "$RACKET_VERSION" = 5.9* ]]; then
     URL="http://download.racket-lang.org/installers/${RACKET_VERSION}/racket-${RACKET_VERSION}-x86_64-linux-ubuntu-quantal.sh"
-elif [[ "$RACKET_VERSION" = 6.* ]]; then
+elif [[ "$RACKET_VERSION" = 6.[0-4]* ]]; then
     URL="http://download.racket-lang.org/installers/${RACKET_VERSION}/racket-${RACKET_VERSION}-x86_64-linux-ubuntu-precise.sh"
+elif [[ "$RACKET_VERSION" = 6.* ]]; then
+    URL="http://download.racket-lang.org/installers/${RACKET_VERSION}/racket-${RACKET_VERSION}-x86_64-linux.sh"
 else
     URL="http://download.racket-lang.org/installers/${RACKET_VERSION}/racket/racket-${RACKET_VERSION}-bin-x86_64-linux-debian-squeeze.sh"
 fi
